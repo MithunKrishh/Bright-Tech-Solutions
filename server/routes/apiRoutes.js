ca14f2f0eef0dd769e81bdcd2af5dda2e8bec05a
Mithun Krishna A <mithunkannan06@gmail.com>
2026-06-14 14:57:30 +0530

    updated json file

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const { getHealthStatus } = require('../controllers/healthController');
-const collegeRoutes = require('./collegeRoutes');
 
 const router = express.Router();
 
 router.get('/health', getHealthStatus);
-router.use('/colleges', collegeRoutes);
 
 module.exports = router;
